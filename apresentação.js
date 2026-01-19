acb92417b7918d0fa113ed0e8aa96dfc50cdf19c
Le-Lucas <56552953+Le-Lucas@users.noreply.github.com>
2026-01-19 00:35:22 -0300

    Refactor presentation.js to add game logic functions

--- apresentação.js
+++ apresentação.js
@@ -1,4 +1,50 @@
-const botaoIniciar = document.querySelector('.botao-iniciar');
-botaoIniciar.addEventListener('click', () => {
-  window.location.href = 'teste.html';
-});
+function mostrarLore() {
+  document.getElementById("intro").classList.add("oculto");
+  document.getElementById("lore").classList.remove("oculto");
+}
+
+function mostrarRegras() {
+  document.getElementById("lore").classList.add("oculto");
+  document.getElementById("regras").classList.remove("oculto");
+}
+let pontos = 10;
+const atributos = { agi:0, fur:0, eng:0, lut:0, esq:0 };
+
+function toggleFicha() {
+  document.getElementById("ficha").classList.toggle("oculto");
+}
+
+function mais(attr) {
+  if (pontos > 0) {
+    atributos[attr]++;
+    pontos--;
+    atualizarFicha();
+  }
+}
+
+function menos(attr) {
+  if (atributos[attr] > 0) {
+    atributos[attr]--;
+    pontos++;
+    atualizarFicha();
+  }
+}
+
+function atualizarFicha() {
+  for (let a in atributos) {
+    document.getElementById(a).textContent = atributos[a];
+  }
+
+  document.getElementById("pontos").textContent =
+    "Pontos disponíveis: " + pontos;
+
+  const btn = document.getElementById("btnDesafio");
+
+  if (pontos === 0) {
+    btn.disabled = false;
+    btn.classList.remove("bloqueado");
+  } else {
+    btn.disabled = true;
+    btn.classList.add("bloqueado");
+  }
+}
